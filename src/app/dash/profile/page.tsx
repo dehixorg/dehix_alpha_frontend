f4d6801c0033d015003ba1832549f6a73fa0f852
abhii23 <rawatab.hii2003@gmail.com>
2024-07-02 23:10:39 +0530

    temp

---
+++ src/app/dash/profile/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ProfileCard from "../profilecard/page";
+import ProjectCard from "@/dashboard/card/page";
+
+
+  const App = () => {
+    return (
+      <div className=" flex container mx-auto px-4 py-8">
+        <div>
+        <ProfileCard 
+        name= "John Doe"
+        email= "john.doe@example.com"
+        />
+        </div>
+        <section className="card-section">
+          <div className="flex justify-center items-center space-x-2">
+            <ProjectCard
+              title="Pending Projects"
+              content="This is a list of pending projects"
+              buttonText="View All"
+            />
+            <ProjectCard
+              title="Completed Projects"
+              content="This is a list of completed projects"
+              buttonText="See More"
+            />
+          </div>
+          
+        </section>
+        
+      </div>
+    );
+  };
+  
+  export default App;  
