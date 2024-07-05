68fd2a1540349b65a8305cf5734e05d7286400d0
sakshammaggu <sakshammaggu80@gmail.com>
2024-07-06 00:23:24 +0530

    changes in profilePage

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -49,7 +49,7 @@ export default function Page() {
     <div className="flex flex-col md:flex-row">
       <ProfileSidebar />
       <div className="bg-gray-800 sm:min-h-screen w-full flex justify-center items-center py-6 md:py-0">
-        <div className="bg-black md:w-fit lg:md-fit sm:w-full rounded-lg flex flex-col items-center justify-center p-4 md:p-8" style={{ height: '98%' }}>
+        <div className="bg-black w-full p-1rem rounded-lg flex flex-col items-center justify-center p-4 md:p-8" style={{ height: '100%' }}>
           <div className="flex flex-col items-center justify-center">
             <section className="flex flex-col items-center justify-center w-full p-6 mt-5 space-y-4 text-white rounded-lg shadow-lg md:ml-5">
               <div className="rounded-full overflow-hidden w-24 h-24 md:w-32 md:h-32 mb-4 bg-gray-700 flex items-center justify-center">
