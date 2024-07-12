e659c9f0baae8eedebce9a3a71e11a1f30a84cff
Abhi Rawat <rawatab.hii23@fmail.com>
2024-07-12 18:24:42 +0530

    education-skillForm

---
+++ src/app/addskillForm/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddSkillForm from "@/components/add-skills/add-skills";
+
+const AddSkillPage = () => {
+  return (
+    <div>
+      <AddSkillForm />
+    </div>
+  );
+};
+
+export default AddSkillPage;
