192d76ac0918ec16861c124bccc197e165200a90
arpit2222 <arpit2018cs164@gmail.com>
2024-07-15 22:07:22 +0530

    adding dummy in json

--- src/app/dashboard/freelancer/page.tsx
+++ src/app/dashboard/freelancer/page.tsx
@@ -40,6 +40,7 @@ import {
   menuItemsTop,
 } from '@/config/menuItems/freelancer/dashboardMenuItems';
 import ProjectTableCard from '@/components/freelancer/homeTableComponent';
+import dummyData from '@/dummydata.json';
 
 interface Project {
   _id: string;
@@ -148,7 +149,7 @@ export default function Dashboard() {
                 x-chunk="dashboard-05-chunk-0"
               >
                 <CardHeader className="pb-3">
-                  <CardTitle className="text-4xl mb-3">$1000</CardTitle>
+                  <CardTitle className="text-4xl mb-3">{dummyData?.freelancerEarnings}</CardTitle>
                 </CardHeader>
                 <CardFooter className=" grid gap-4 grid-cols-4">
                   <div className="col-span-3">
