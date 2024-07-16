aa75fdf5b43e46b0754fa7ae57e10c91e7087414
Abhi Rawat <rawatab.hii23@fmail.com>
2024-07-16 21:33:33 +0530

    talent form toggle button

--- src/components/dash-comp/talentForm/skilldomainForm.tsx
+++ src/components/dash-comp/talentForm/skilldomainForm.tsx
@@ -17,6 +17,7 @@ import {
 } from '@/components/ui/dialog';
 import { Card } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
 import {
   Table,
   TableHeader,
@@ -151,12 +152,6 @@ const SkillDomainForm: React.FC = () => {
     ]);
     setStatusVisibility([...statusVisibility, false]);
     resetDomainForm();
-  };
-
-  const toggleStatusVisibility = (index: number) => {
-    const updatedVisibility = [...statusVisibility];
-    updatedVisibility[index] = !updatedVisibility[index];
-    setStatusVisibility(updatedVisibility);
   };
 
   return (
@@ -352,31 +347,7 @@ const SkillDomainForm: React.FC = () => {
                   <TableCell>{data.monthlyPay}</TableCell>
                   <TableCell>{data.status}</TableCell>
                   <TableCell>
-                    <label
-                      htmlFor={`statusVisibility${index}`}
-                      className="inline-flex items-center cursor-pointer"
-                    >
-                      <input
-                        type="checkbox"
-                        id={`statusVisibility${index}`}
-                        value=""
-                        className="sr-only peer"
-                        checked={statusVisibility[index]}
-                        onChange={() => toggleStatusVisibility(index)}
-                        aria-labelledby={`statusVisibilityLabel${index}`}
-                      />
-                      <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                      <span
-                        className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        style={{ width: '50px', textAlign: 'center' }}
-                      ></span>
-                      <span
-                        id={`statusVisibilityLabel${index}`}
-                        className="sr-only"
-                      >
-                        Toggle Status Visibility
-                      </span>
-                    </label>
+                    <Switch />
                   </TableCell>
                 </TableRow>
               ))}
