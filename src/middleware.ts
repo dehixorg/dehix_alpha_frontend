4e6a62593218370af8cd505efd8bdb65848e9037
arpit2222 <arpit2018cs164@gmail.com>
2024-07-19 03:23:18 +0530

    solved comment issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,30 +8,30 @@ export async function middleware(request: NextRequest) {
   const token = cookies.token;
 
   const { pathname } = request.nextUrl;
-  // if (token && userType) {
-  //   if (
-  //     userType === 'freelancer' &&
-  //     pathname.startsWith('/dashboard/business')
-  //   ) {
-  //     return NextResponse.redirect(
-  //       new URL('/dashboard/freelancer', request.url),
-  //     );
-  //   } else if (
-  //     userType === 'business' &&
-  //     pathname.startsWith('/dashboard/freelancer')
-  //   ) {
-  //     return NextResponse.redirect(new URL('/dashboard/business', request.url));
-  //   }
-  // } else {
-  //   const protectedRoutes = [
-  //     '/dashboard',
-  //     '/dashboard/business',
-  //     '/dashboard/freelancer',
-  //   ];
-  //   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-  //     return NextResponse.redirect(new URL('/', request.url));
-  //   }
-  // }
+  if (token && userType) {
+    if (
+      userType === 'freelancer' &&
+      pathname.startsWith('/dashboard/business')
+    ) {
+      return NextResponse.redirect(
+        new URL('/dashboard/freelancer', request.url),
+      );
+    } else if (
+      userType === 'business' &&
+      pathname.startsWith('/dashboard/freelancer')
+    ) {
+      return NextResponse.redirect(new URL('/dashboard/business', request.url));
+    }
+  } else {
+    const protectedRoutes = [
+      '/dashboard',
+      '/dashboard/business',
+      '/dashboard/freelancer',
+    ];
+    if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
   return NextResponse.next();
 }
 
