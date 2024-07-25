16eea8c7cc94965160ff52b6f32cd54b44542bb8
Abhi Rawat <rawatab.hii23@fmail.com>
2024-07-26 00:17:23 +0530

    add-businessProfile-link

--- src/components/shared/DropdownProfile.tsx
+++ src/components/shared/DropdownProfile.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { UserIcon, LogOut } from 'lucide-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouter } from 'next/navigation';
@@ -21,6 +22,19 @@ export default function DropdownProfile() {
   const user = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
   const router = useRouter();
+
+  const [userType, setUserType] = useState<string | null>(null); // Added userType state
+
+  useEffect(() => {
+    // Check if user type is available in Redux store
+    if (user?.type) {
+      setUserType(user.type);
+    } else {
+      // If not, get it from cookies
+      const storedUserType = Cookies.get('userType');
+      setUserType(storedUserType || null);
+    }
+  }, [user]);
 
   const handleLogout = () => {
     dispatch(clearUser());
@@ -48,9 +62,19 @@ export default function DropdownProfile() {
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <Link href="/settings/personal-info">
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-        </Link>
+        <div>
+          {userType === 'freelancer' ? (
+            <Link href="/settings/personal-info">
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+            </Link>
+          ) : userType === 'business' ? (
+            <Link href="/settings/business-info">
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+            </Link>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
         <Link href="/settings/support">
           <DropdownMenuItem>Support</DropdownMenuItem>
         </Link>
