3e3504f7b5d9b2ee5e11ab4d37edf953cfba3e13
Abhi Rawat <rawatab.hii23@fmail.com>
2024-08-02 11:22:22 +0530

    change

--- src/app/businessProject/[project_id]/page.tsx
+++ src/app/businessProject/[project_id]/page.tsx
@@ -10,13 +10,16 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from '@/components/ui/card';
 import DropdownProfile from '@/components/shared/DropdownProfile';
 import { Input } from '@/components/ui/input';
 import ProjectDetailCard from '@/components/freelancer/project/projectDetailCard';
 import SidebarMenu from '@/components/menu/sidebarMenu';
 import CollapsibleSidebarMenu from '@/components/menu/collapsibleSidebarMenu';
-import { menuItemsTop, menuItemsBottom } from '@/config/menuItems/business/dashboardMenuItems';
+import {
+  menuItemsTop,
+  menuItemsBottom,
+} from '@/config/menuItems/business/dashboardMenuItems';
 import { axiosInstance } from '@/lib/axiosinstance';
 import ProjectSkillCard from '@/components/business/projectSkillCard';
 
@@ -58,7 +61,9 @@ export default function Dashboard() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axiosInstance.get(`/business/${project_id}/project`);
+        const response = await axiosInstance.get(
+          `/business/${project_id}/project`,
+        );
         console.log(response.data.data);
         setProject(response.data.data);
       } catch (error) {
@@ -74,10 +79,18 @@ export default function Dashboard() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <SidebarMenu menuItemsTop={menuItemsTop} menuItemsBottom={menuItemsBottom} active="Dashboard" />
+      <SidebarMenu
+        menuItemsTop={menuItemsTop}
+        menuItemsBottom={menuItemsBottom}
+        active="Dashboard"
+      />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-          <CollapsibleSidebarMenu menuItemsTop={menuItemsTop} menuItemsBottom={menuItemsBottom} active="Dashboard" />
+          <CollapsibleSidebarMenu
+            menuItemsTop={menuItemsTop}
+            menuItemsBottom={menuItemsBottom}
+            active="Dashboard"
+          />
           <Breadcrumb
             items={[
               { label: 'Dashboard', link: '/dashboard/business' },
@@ -86,7 +99,11 @@ export default function Dashboard() {
           />
           <div className="relative ml-auto flex-1 md:grow-0">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input type="search" placeholder="Search..." className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]" />
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+            />
           </div>
           <DropdownProfile />
         </header>
