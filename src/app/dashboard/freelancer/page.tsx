84a614dc90ea2bbdf3731a73eb153d354ec17832
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-10-14 21:12:52 +0530

    Disabled button

--- src/app/dashboard/freelancer/page.tsx
+++ src/app/dashboard/freelancer/page.tsx
@@ -259,7 +259,7 @@ export default function Dashboard() {
             <div className="text-center py-10">
               <CalendarX2 className="mx-auto mb-2 text-gray-500" size="100" />
               <p className="text-gray-500">No interviews scheduled</p>
-              <Button className="mt-3" onClick={handleAuth}>
+              <Button className="mt-3" onClick={handleAuth} disabled>
                 Create Meet
               </Button>
             </div>
