50437250b1f207d9e32f30f56226dce6970c9dd5
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-10-22 02:24:29 +0530

    Added s3 remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['de-test-bucket-8285.s3.ap-south-1.amazonaws.com'], // Add your S3 bucket domain here
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'de-test-bucket-8285.s3.ap-south-1.amazonaws.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
     },
   };
   
