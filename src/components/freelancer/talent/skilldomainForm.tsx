159fd4e7c2530efc1c1081e7affddd43ba1d9ced
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-11-09 13:32:43 +0530

    Fixed date range

--- src/components/freelancer/talent/skilldomainForm.tsx
+++ src/components/freelancer/talent/skilldomainForm.tsx
@@ -140,7 +140,7 @@ const SkillDomainForm: React.FC = () => {
   return (
     <div className="p-6 mt-2">
       <div className="mb-8 mt-1 ml-2">
-        <h1 className="text-3xl font-bold">Dehix Talent </h1>
+        <h1 className="text-3xl font-bold">Dehix Talent</h1>
         <p className="text-gray-400 mt-2">
           Here you can Add relevant skills and domains to get directly hired
           from dehix talent.
@@ -194,9 +194,7 @@ const SkillDomainForm: React.FC = () => {
                       <TableCell>${item.monthlyPay}</TableCell>
                       <TableCell>
                         <Badge className={getBadgeColor(item.status)}>
-                          {item.status.replace(/\b\w/g, (char) =>
-                            char.toUpperCase(),
-                          )}
+                          {item.status.toUpperCase()}
                         </Badge>
                       </TableCell>
                       <TableCell>
