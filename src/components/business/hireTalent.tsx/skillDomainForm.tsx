ba2a0b77d3b5496c77560924e15418b1a9291ea1
tushar2708 <rtushar2708@gmail.com>
2024-11-09 18:07:41 +0530

    dont show skills and domains which are used

--- src/components/business/hireTalent.tsx/skillDomainForm.tsx
+++ src/components/business/hireTalent.tsx/skillDomainForm.tsx
@@ -58,20 +58,6 @@ const SkillDomainForm: React.FC<SkillDomainFormProps> = ({
   // Fetch skills, domains, and user's skill/domain data
   const fetchData = useCallback(async () => {
     try {
-      const skillsResponse = await axiosInstance.get('/skills/all');
-      if (skillsResponse?.data?.data) {
-        setSkills(skillsResponse.data.data);
-      } else {
-        throw new Error('Skills response is null or invalid');
-      }
-      const domainsResponse = await axiosInstance.get('/domain/all');
-      if (domainsResponse?.data?.data) {
-        setDomains(domainsResponse.data.data);
-      } else {
-        throw new Error('Domains response is null or invalid');
-      }
-
-      // Fetch the skill/domain data for the specific freelancer
       if (user?.uid) {
         const hireTalentResponse = await axiosInstance.get(
           `/business/${user.uid}/hireDehixTalent`,
@@ -112,6 +98,46 @@ const SkillDomainForm: React.FC<SkillDomainFormProps> = ({
         setStatusVisibility(
           formattedHireTalentData.map((item) => item.visible),
         );
+
+        const filterSkills = hireTalentData
+          .filter((item: any) => item.skillName)
+          .map((item: any) => ({
+            _id: item.skillId,
+            label: item.skillName,
+          }));
+
+        const filterDomains = hireTalentData
+          .filter((item: any) => item.domainName)
+          .map((item: any) => ({
+            _id: item.domainId,
+            label: item.domainName,
+          }));
+
+        // fetch skills and domains data
+        const skillsResponse = await axiosInstance.get('/skills/all');
+        if (skillsResponse?.data?.data) {
+          const uniqueSkills = skillsResponse.data.data.filter(
+            (skill: any) =>
+              !filterSkills.some(
+                (filterSkill: any) => filterSkill._id === skill._id,
+              ),
+          );
+          setSkills(uniqueSkills);
+        } else {
+          throw new Error('Skills response is null or invalid');
+        }
+        const domainsResponse = await axiosInstance.get('/domain/all');
+        if (domainsResponse?.data?.data) {
+          const uniqueDomain = domainsResponse.data.data.filter(
+            (domain: any) =>
+              !filterDomains.some(
+                (filterDomain: any) => filterDomain._id === domain._id,
+              ),
+          );
+          setDomains(uniqueDomain);
+        } else {
+          throw new Error('Domains response is null or invalid');
+        }
       }
     } catch (error: any) {
       console.error('Error fetching data:', error);
