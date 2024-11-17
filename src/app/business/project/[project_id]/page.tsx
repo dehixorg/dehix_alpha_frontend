128250bb16c7bc9f7549c658e27637b6e5dbc304
tushar2708 <rtushar2708@gmail.com>
2024-11-17 21:58:04 +0530

    fix

--- src/app/business/project/[project_id]/page.tsx
+++ src/app/business/project/[project_id]/page.tsx
@@ -4,11 +4,7 @@ import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import Breadcrumb from '@/components/shared/breadcrumbList';
-import {
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import DropdownProfile from '@/components/shared/DropdownProfile';
 import { Input } from '@/components/ui/input';
 import ProjectDetailCard from '@/components/freelancer/project/projectDetailCard';
