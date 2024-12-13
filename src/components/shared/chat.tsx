5bd70fe1af5eb359bd085e1bff429528af4ff2a5
akhil.chaudhary <akhil.chaudhary@equipmentshare.com>
2024-12-13 17:09:02 +0530

    Styled chats

--- src/components/shared/chat.tsx
+++ src/components/shared/chat.tsx
@@ -235,7 +235,7 @@ export function CardsChat({ conversation }: CardsChatProps) {
           <LoaderCircle className="h-6 w-6 text-white animate-spin" />
         </div>
       ) : (
-        <Card className="col-span-2">
+        <Card className="col-span-2 max-h-[85vh]">
           <CardHeader className="flex flex-row items-center">
             <div className="flex items-center space-x-4">
               <Avatar>
@@ -252,9 +252,10 @@ export function CardsChat({ conversation }: CardsChatProps) {
               </div>
             </div>
           </CardHeader>
-          <CardContent className="flex-1 overflow-y-auto px-4 py-2 space-y-4">
-            {/* Show loading spinner while fetching data */}
-            <div className="space-y-4 h-[540px]">
+          <CardContent className="flex-1 px-6 pb-4">
+            {/* Scrollable messages container */}
+            <div className="space-y-4 overflow-y-auto max-h-[60vh]">
+              {/* Show loading spinner while fetching data */}
               {messages.map((message, index) => {
                 const readableTimestamp =
                   formatDistanceToNow(new Date(message.timestamp)) + ' ago';
@@ -306,7 +307,8 @@ export function CardsChat({ conversation }: CardsChatProps) {
               })}
             </div>
           </CardContent>
-          <CardFooter className="pt-4">
+
+          <CardFooter>
             <form
               onSubmit={(event) => {
                 event.preventDefault();
