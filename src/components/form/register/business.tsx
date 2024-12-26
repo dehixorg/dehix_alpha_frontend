a54e9a4b0601c6d090314930d12f79e08f591b2d
Harshit Kharayat <harshitkharayat224@gmail.com>
2024-12-26 18:41:00 +0530

    resolved the confllct

--- src/components/form/register/business.tsx
+++ src/components/form/register/business.tsx
@@ -255,7 +255,6 @@ export default function BusinessRegisterForm() {
               className="w-full"
             />
           </div>
-          
 
           <div className="grid gap-2">
             <TextInput
