48e779c49784947af1d76f2241e223b27e05cf3b
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2024-12-27 15:59:39 +0000

    more changes

--- src/components/form/register/freelancer.tsx
+++ src/components/form/register/freelancer.tsx
@@ -91,7 +91,8 @@ export default function FreelancerRegisterForm() {
   const [phone, setPhone] = useState<string>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [passwordStrength, setPasswordStrength] = useState<string>('');
-  const [passwordStrengthClass, setPasswordStrengthClass] = useState<string>('');
+  const [passwordStrengthClass, setPasswordStrengthClass] =
+    useState<string>('');
 
   const formRef = useRef<HTMLFormElement>(null);
   const searchParams = useSearchParams();
@@ -122,15 +123,15 @@ export default function FreelancerRegisterForm() {
     setShowPassword((prev) => !prev);
   };
 
-  const checkPasswordStrength = (password:string) => {
+  const checkPasswordStrength = (password: string) => {
     let strength = '';
     let className = '';
 
     const strongRegex = new RegExp(
-      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{12,}$'
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{12,}$',
     );
     const mediumRegex = new RegExp(
-      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d!@#$%^&*]{8,}$'
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d!@#$%^&*]{8,}$',
     );
 
     if (strongRegex.test(password)) {
@@ -277,56 +278,58 @@ export default function FreelancerRegisterForm() {
             type="url"
             placeholder="Enter your Resume Google Drive Link"
           />
-      <div className="space-y-2">
-      <Label>Password</Label>
-      <div className="relative">
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <div className="relative">
-                  <Input
-                    placeholder="Enter your password"
-                    type={showPassword ? 'text' : 'password'}
-                    {...field}
-                    onChange={(e) => {
-                      field.onChange(e);
-                      checkPasswordStrength(e.target.value);
-                    }}
-                  />
-                  <button
-                    type="button"
-                    onClick={togglePasswordVisibility}
-                    className="absolute inset-y-0 right-0 px-3 flex items-center"
-                  >
-                    {showPassword ? (
-                      <Eye className="h-5 w-5" />
-                    ) : (
-                      <EyeOff className="h-5 w-5" />
-                    )}
-                  </button>
-                </div>
-              </FormControl>
-              <FormDescription>
-                Password must:
-                <ul className="list-disc ml-4 mt-1 text-sm text-gray-600">
-                  <li>Be at least 12 characters long</li>
-                  <li>Include uppercase and lowercase letters</li>
-                  <li>Contain numbers and special characters</li>
-                </ul>
-              </FormDescription>
-              <div className="mt-2 text-sm text-gray-600">
-                Password Strength:{' '}
-                <span className={passwordStrengthClass}>{passwordStrength}</span>
-              </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </div>
-    </div>
+          <div className="space-y-2">
+            <Label>Password</Label>
+            <div className="relative">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          placeholder="Enter your password"
+                          type={showPassword ? 'text' : 'password'}
+                          {...field}
+                          onChange={(e) => {
+                            field.onChange(e);
+                            checkPasswordStrength(e.target.value);
+                          }}
+                        />
+                        <button
+                          type="button"
+                          onClick={togglePasswordVisibility}
+                          className="absolute inset-y-0 right-0 px-3 flex items-center"
+                        >
+                          {showPassword ? (
+                            <Eye className="h-5 w-5" />
+                          ) : (
+                            <EyeOff className="h-5 w-5" />
+                          )}
+                        </button>
+                      </div>
+                    </FormControl>
+                    <FormDescription>
+                      Password must:
+                      <ul className="list-disc ml-4 mt-1 text-sm text-gray-600">
+                        <li>Be at least 12 characters long</li>
+                        <li>Include uppercase and lowercase letters</li>
+                        <li>Contain numbers and special characters</li>
+                      </ul>
+                    </FormDescription>
+                    <div className="mt-2 text-sm text-gray-600">
+                      Password Strength:{' '}
+                      <span className={passwordStrengthClass}>
+                        {passwordStrength}
+                      </span>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
           <div className="grid grid-cols-2 gap-4 mt-3">
             <div className="grid gap-2">
               <FormField
