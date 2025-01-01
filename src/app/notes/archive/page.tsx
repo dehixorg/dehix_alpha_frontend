2c9ef7519685411a41c4ea77aeae09089357a0d4
algowhiz <algowhiz@gmail.com>
2025-01-01 22:39:36 +0530

    lint-fix error solve

--- src/app/notes/archive/page.tsx
+++ src/app/notes/archive/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Note } from '@/utils/types/note';
