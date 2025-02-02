f25e75a7b5d5b3c6fb0a44b9bd638fbdef850f51
tushar2708 <rtushar2708@gmail.com>
2025-02-02 23:26:12 +0530

    remove auto redirect to auth/login after 1.5 sec

--- src/components/form/register/freelancer.tsx
+++ src/components/form/register/freelancer.tsx
@@ -322,9 +322,11 @@ function FreelancerRegisterForm({
         description: 'Redirecting to login page...',
       });
 
-      setTimeout(() => {
-        router.push('/auth/login');   
-      }, 1500);
+      setIsModalOpen(true);
+
+      // setTimeout(() => {
+      //   router.push('/auth/login');
+      // }, 1500);
     } catch (error: any) {
       const errorMessage =
         error.response?.data?.message || 'Something went wrong!';
