5de17e8061a5f7d014b1b9868bf4ac86fba2259a
ManishCody <algowhiz@gmail.com>
2025-02-03 18:02:06 +0530

    code updates

--- src/app/business/market/page.tsx
+++ src/app/business/market/page.tsx
@@ -100,6 +100,7 @@ const Market: React.FC = () => {
     try {
       const queryString = constructQueryString(appliedFilters);
       const response = await axiosInstance.get(`/freelancer?${queryString}`);
+      console.log(response);
 
       setFreelancers(response.data.data);
     } catch (error) {
