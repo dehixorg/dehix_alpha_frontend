d01f4dfe78c0baaf24ca9c39a1482e80f736e803
ManishCody <algowhiz@gmail.com>
2025-03-08 06:05:11 +0530

    code update

--- src/utils/NetworkProvider.tsx
+++ src/utils/NetworkProvider.tsx
@@ -1,18 +1,44 @@
 "use client";
 
-import OfflinePage from "@/components/shared/OfflinePage";
+import { useEffect } from "react";
 import { useNetwork } from "@/hooks/useNetwork";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import { WifiOff } from "lucide-react";
 
 export default function NetworkProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-    const isOnline = useNetwork();
+  const isOnline = useNetwork();
+  const { toast, dismiss } = useToast();
 
+  useEffect(() => {
     if (!isOnline) {
-      return <OfflinePage isOnline={isOnline} />;
+      toast({
+        title: (
+          <div className="flex items-center gap-2">
+            <WifiOff className="w-6 h-6 text-white animate-pulse" />
+            <span>Internet Disconnected</span>
+          </div>
+        ) as any,
+        description: "Please check your connection.",
+        variant: "destructive",
+        duration: Infinity,
+      });
+
+      document.body.style.pointerEvents = "none";
+    } else {
+      dismiss(); 
+      document.body.style.pointerEvents = "auto";
     }
-  
-    return <>{children}</>;
+  }, [isOnline]);
+
+  return (
+    <>
+      {children}
+      <Toaster />
+    </>
+  );
 }
