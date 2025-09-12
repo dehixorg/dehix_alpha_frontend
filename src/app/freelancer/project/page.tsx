ef66a88409d2ebf34141f95aec914ca80781ec45
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-09-13 00:36:21 +0530

    Created the Layout with sidebar and header

--- src/app/freelancer/project/page.tsx
+++ src/app/freelancer/project/page.tsx
@@ -1,5 +1,11 @@
 'use client';
-import { PackageOpen } from 'lucide-react';
+import {
+  PackageOpen,
+  FolderDot,
+  Pointer,
+  FileCheck,
+  CircleX,
+} from 'lucide-react';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
@@ -10,6 +16,16 @@ import { ProjectCard } from '@/components/cards/projectCard';
 import { StatusEnum } from '@/utils/freelancer/enum';
 import { toast } from '@/components/ui/use-toast';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import SidebarMenu from '@/components/menu/sidebarMenu';
+import Header from '@/components/header/header';
+import {
+  menuItemsBottom as freelancerMenuItemsBottom,
+  menuItemsTop as freelancerMenuItemsTop,
+} from '@/config/menuItems/freelancer/dashboardMenuItems';
+import {
+  menuItemsBottom,
+  menuItemsTop,
+} from '@/config/menuItems/business/dashboardMenuItems';
 
 interface Project {
   _id: string;
@@ -76,7 +92,7 @@ const ProjectList = ({
   }, [user.uid, status, projectType]);
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex w-full flex-col">
       <div className="flex flex-col sm:gap-8 sm:py-0 mb-8">
         {isLoading ? (
           <div
@@ -136,47 +152,146 @@ export default function ProjectPage() {
   const [projectType, setProjectType] = useState('CONSULTANT');
 
   return (
-    <Tabs defaultValue="current" className="w-full">
-      <TabsList>
-        <TabsTrigger value="current">Current</TabsTrigger>
-        <TabsTrigger value="applied">Applied</TabsTrigger>
-        <TabsTrigger value="completed">Completed</TabsTrigger>
-        <TabsTrigger value="rejected">Rejected</TabsTrigger>
-      </TabsList>
-      <div className="flex justify-between items-center mb-8 ml-6 pr-6">
-        <div>
-          <h1 className="text-3xl font-bold">Current Projects</h1>
-          <p className="text-gray-400 mt-2">
-            Browse and manage your active freelance projects
-          </p>
-        </div>
-        <div className="flex items-center gap-2">
-          <label htmlFor="project-type" className="text-gray-600 font-medium">
-            Filter by:
-          </label>
-          <select
-            id="project-type"
-            value={projectType}
-            onChange={(e) => setProjectType(e.target.value)}
-            className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            <option value="CONSULTANT">Consultant</option>
-            <option value="FREELANCER">Freelancer</option>
-          </select>
+    <div className="flex min-h-screen bg-background">
+      <div className="bg-background border-r">
+        <SidebarMenu
+          menuItemsTop={freelancerMenuItemsTop}
+          menuItemsBottom={freelancerMenuItemsBottom}
+          active="Projects"
+        />
+      </div>
+      <div className="flex mb-8 flex-col sm:pl-14 w-full">
+        <Header
+          menuItemsTop={menuItemsTop}
+          menuItemsBottom={menuItemsBottom}
+          activeMenu="Dashboard"
+          breadcrumbItems={[
+            { label: 'Freelancer', link: '/dashboard/freelancer' },
+            { label: 'Projects', link: '/freelancer/project' },
+          ]}
+        />
+        <div className="flex-1">
+          <div className="w-full p-4">
+            <Tabs defaultValue="current" className="w-full flex flex-col gap-4">
+              <TabsList className="grid w-full grid-cols-4">
+                <TabsTrigger
+                  value="current"
+                  className="flex items-center gap-2"
+                >
+                  <FolderDot className="h-4 w-4" />
+                  Current
+                </TabsTrigger>
+                <TabsTrigger
+                  value="applied"
+                  className="flex items-center gap-2"
+                >
+                  <Pointer className="h-4 w-4" />
+                  Applied
+                </TabsTrigger>
+                <TabsTrigger
+                  value="completed"
+                  className="flex items-center gap-2"
+                >
+                  <FileCheck className="h-4 w-4" />
+                  Completed
+                </TabsTrigger>
+                <TabsTrigger
+                  value="rejected"
+                  className="flex items-center gap-2"
+                >
+                  <CircleX className="h-4 w-4" />
+                  Rejected
+                </TabsTrigger>
+              </TabsList>
+
+              <TabsContent value="current">
+                <div className="flex justify-between items-center mb-8 ml-6 pr-6">
+                  <div>
+                    <h1 className="text-3xl font-bold">Current Projects</h1>
+                    <p className="text-gray-400 mt-2">
+                      Browse and manage your active freelance projects
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <label
+                      htmlFor="project-type"
+                      className="text-gray-600 font-medium"
+                    >
+                      Filter by:
+                    </label>
+                    <select
+                      id="project-type"
+                      value={projectType}
+                      onChange={(e) => setProjectType(e.target.value)}
+                      className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                      <option value="CONSULTANT">Consultant</option>
+                      <option value="FREELANCER">Freelancer</option>
+                    </select>
+                  </div>
+                </div>
+                <ProjectList status="ACTIVE" projectType={projectType} />
+              </TabsContent>
+
+              <TabsContent value="applied">
+                <div className="flex justify-between items-center mb-8 ml-6 pr-6">
+                  <div>
+                    <h1 className="text-3xl font-bold">Applied Projects</h1>
+                    <p className="text-gray-400 mt-2">
+                      Track the status of your projects currently undergoing
+                      verification before final approval.
+                    </p>
+                  </div>
+                </div>
+                <ProjectList status="PENDING" />
+              </TabsContent>
+
+              <TabsContent value="completed">
+                <div className="flex justify-between items-center mb-8 ml-6 pr-6">
+                  <div>
+                    <h1 className="text-3xl font-bold">Completed Projects</h1>
+                    <p className="text-gray-400 mt-2">
+                      Explore and manage your successfully completed freelance
+                      projects.
+                    </p>
+                  </div>
+                </div>
+                <ProjectList status="COMPLETED" />
+              </TabsContent>
+
+              <TabsContent value="rejected">
+                <div className="flex justify-between items-center mb-8 ml-6 pr-6">
+                  <div>
+                    <h1 className="text-3xl font-bold">Rejected Projects</h1>
+                    <p className="text-gray-400 mt-2">
+                      Explore and Review projects that were not selected and
+                      gain insights for future submissions.
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <label
+                      htmlFor="project-type"
+                      className="text-gray-600 font-medium"
+                    >
+                      Filter by:
+                    </label>
+                    <select
+                      id="project-type"
+                      value={projectType}
+                      onChange={(e) => setProjectType(e.target.value)}
+                      className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                      <option value="CONSULTANT">Consultant</option>
+                      <option value="FREELANCER">Freelancer</option>
+                    </select>
+                  </div>
+                </div>
+                <ProjectList status="REJECTED" projectType={projectType} />
+              </TabsContent>
+            </Tabs>
+          </div>
         </div>
       </div>
-      <TabsContent value="current">
-        <ProjectList status="ACTIVE" projectType={projectType} />
-      </TabsContent>
-      <TabsContent value="applied">
-        <ProjectList status="PENDING" />
-      </TabsContent>
-      <TabsContent value="completed">
-        <ProjectList status="COMPLETED" />
-      </TabsContent>
-      <TabsContent value="rejected">
-        <ProjectList status="REJECTED" projectType={projectType} />
-      </TabsContent>
-    </Tabs>
+    </div>
   );
 }
