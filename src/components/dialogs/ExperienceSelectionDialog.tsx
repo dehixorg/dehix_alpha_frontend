e1cfa56562c90fa6a720849ea857de4ddee8b4bf
Akhil Chaudhary <akhilcodebugged@gmail.com>
2025-10-02 00:10:51 +0530

    fixes

--- src/components/dialogs/ExperienceSelectionDialog.tsx
+++ src/components/dialogs/ExperienceSelectionDialog.tsx
@@ -11,7 +11,6 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Badge } from '@/components/ui/badge';
 import { notifyError, notifySuccess } from '@/utils/toastMessage';
 import { axiosInstance } from '@/lib/axiosinstance';
 
