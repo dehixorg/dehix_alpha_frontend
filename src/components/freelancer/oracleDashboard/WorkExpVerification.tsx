b439f2c7a9beed00c9adedc9b3c63a34dabc46da
Akhil <91989976a@gmail.com>
2025-10-16 08:46:15 -0700

    fixes

--- src/components/freelancer/oracleDashboard/WorkExpVerification.tsx
+++ src/components/freelancer/oracleDashboard/WorkExpVerification.tsx
@@ -177,14 +177,13 @@ const WorkExpVerification = () => {
           (t) => (
             <TabsContent key={t} value={t}>
               <CardContent>
-                <div className="grid flex-1 items-start gap-4 md:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <div className="grid flex-1 items-start gap-4 md:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                   {filteredData.length > 0 ? (
                     filteredData.map((data) => (
                       <WorkExpVerificationCard
                         key={data.document_id}
                         _id={data._id}
                         jobTitle={data.jobTitle}
-                        workDescription={data.workDescription}
                         company={data.company}
                         startFrom={data.workFrom}
                         endTo={data.workTo}
