069a4953f094eb1b1518cd71c2ce399ac09b67f9
Atul Rajendra Bodke <126400018+Atulbodke408@users.noreply.github.com>
2025-11-02 16:57:14 +0530

    Uifix

--- src/components/freelancer/interview/Profile.tsx
+++ src/components/freelancer/interview/Profile.tsx
@@ -9,12 +9,14 @@ export default function ProfileComponent() {
   const user = useSelector((state: RootState) => state.user);
   return (
     <>
-      <div className="ml-5 md:ml-10">
-        <h1 className="text-3xl font-bold">Interview Profile</h1>
-        <p className="text-gray-400 mt-2">
-          Manage and track your skills and domains. Add new skills or domains
-          and provide your experience levels.
+      <div className="ml-5 md:ml-10 mr-5 md:mr-10">
+        <div className="w-full rounded-xl p-5 md:p-6 bg-gradient-to-r from-blue-600 via-blue-500 to-cyan-500">
+          <h1 className="text-2xl md:text-3xl font-bold text-white">Interview Profile</h1>
+          <p className="mt-2 text-slate-200">
+            Manage and track your skills or domains. Add new skills or domains and provide your
+            experience levels.
         </p>
+      </div>
       </div>
       <InterviewProfile freelancerId={user?.uid} />
     </>
